fe495b5f760f826032b7f5e51adfb90ccd4bd035
Jack Morrissey <j.morrissey.16@gmail.com>
2018-11-27 00:57:42 -0600

    route work

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// api routes
+router.use("/api", apiRoutes);
+
+// default - if no routs hit send home page
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
