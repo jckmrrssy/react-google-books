c3fba164f607c9cb580f4fd5782da61c2eb2fadf
Jack Morrissey <j.morrissey.16@gmail.com>
2018-11-28 12:37:22 -0600

    updating router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import {BrowserRouter, Route} from 'react-router-dom';
-import Books from './pages/Books';
-import Saved from './pages/Saved';
+import Books from './pages/Books/Books';
+import Saved from './pages/Saved/Saved';
 import "./App.css";
 
 const App = () => (
   <BrowserRouter>
     <div>
         <Route exact path = '/' component = {Books} />
-        <Route exact path = '/books' component = {Books} />
-        <Route exact path = '/books/:id' component = {Saved} />
+        <Route exact path = '/books' component = {Saved} />
     </div>
   </BrowserRouter>
-)
+);
 
 export default App;
